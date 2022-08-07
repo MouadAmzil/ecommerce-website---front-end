79cf8c045f6f9b617ba7c037b09331f307a24799
OssamaAourfi <aourfidev@gmail.com>
2022-08-07 11:53:29 +0100

    detail

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-<<<<<<< HEAD
+
 <v-app>
             <router-view />
 
 </v-app>
-=======
-  <v-app>
-    <router-view />
-  </v-app>
->>>>>>> e65523db7022c8e3e0c243c3c6573dcdf715213b
+
+ 
 </template>
 
 <script>
@@ -19,20 +16,14 @@ import DetailProduit from './views/DetailProduit.vue';
 
 export default {
   name: "App",
-<<<<<<< HEAD
+
 components:{
 Login,
 AboutView,
 SignUp,
 DetailProduit
 },
-=======
-  components: {
-    Login,
-    AboutView,
-    SignUp,
-  },
->>>>>>> e65523db7022c8e3e0c243c3c6573dcdf715213b
+
   data() {
     return {
       login: false,
