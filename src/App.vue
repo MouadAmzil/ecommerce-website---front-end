b7d3cde69700e0eb87c4177cf8d8dc9491f9dba8
MouadAmzil <mouadmouad2026@gmail.com>
2022-08-07 03:35:17 +0100

    Card is Done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-            <router-view />
+  <v-app>
+    <router-view />
+  </v-app>
 </template>
 
-<script> 
+<script>
 import Login from './views/Login/LoginView.vue';
 import AboutView from './views/HomeView.vue';
 import SignUp from './views/Login/SignupView.vue';
 
 export default {
   name: "App",
-components:{
-Login,
-AboutView,
-SignUp,
-},
+  components: {
+    Login,
+    AboutView,
+    SignUp,
+  },
   data() {
     return {
       login: false,
