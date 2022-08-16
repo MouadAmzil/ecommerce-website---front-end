2d77269a1c4335a4ec42ee19c9da2b4769aa0eef
MouadAmzil <mouadmouad2026@gmail.com>
2022-08-17 00:30:36 +0100

    .

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -130,6 +130,25 @@
 .img {
   color: white !important;
 }
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style> 
 <script>
 export default {
