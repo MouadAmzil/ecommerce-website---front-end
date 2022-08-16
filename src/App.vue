e162ce26ca871facc8219988a7a7be0f16c77b48
MouadAmzil <mouadmouad2026@gmail.com>
2022-08-17 00:38:07 +0100

    Add some style for scroll

--- src/App.vue
+++ src/App.vue
@@ -171,6 +171,25 @@
 .img {
   color: white !important;
 }
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
 <script>
 import login from '../src/views/Login/LoginView.vue'
--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -131,24 +131,6 @@
   color: white !important;
 }
 
-::-webkit-scrollbar {
-  width: 10px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #888;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
 </style> 
 <script>
 export default {
