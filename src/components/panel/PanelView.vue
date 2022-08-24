e7be558c826968399ba53f75d97b7265884427c6
MouadAmzil <mouadmouad2026@gmail.com>
2022-08-24 02:07:17 +0100

    Add pannel

--- src/components/panel/PanelView.vue
+++ src/components/panel/PanelView.vue
@@ -1,83 +1,130 @@
 <template>
-    <v-spacer class="pt-16 px-3">
-      <v-row>
-         <v-col max-width="100%" max-height="100%" cols="8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatibus earum aspernatur dolor, iure placeat animi iusto quaerat suscipit repellendus, modi necessitatibus rerum perferendis vitae doloremque, quia itaque eius repellat!
-          Voluptates quibusdam nisi tempore deserunt quia rem delectus repellat illo qui, ea inventore reprehenderit, ex odio sint autem exercitationem blanditiis consectetur nam harum culpa aspernatur porro. Facere eaque eos facilis?
-          Voluptatem quidem expedita laudantium ipsam optio? Nam, itaque doloremque ducimus sunt hic aliquam voluptatibus aperiam! Impedit at est deserunt recusandae reprehenderit obcaecati neque veniam aspernatur, vero eligendi maxime fugiat sunt.
-          Distinctio, quidem. V sit facere blanditiis voluptatem saepe, eos esse nobis quaerat laboriosam labore recusandae aliquid? Vel officia temporibus dolorum eligendi sapiente rerum? Vel.
-          Autem, quas soluta praesentium quisquam distinctio voluptas quibusdam ipsum voluptates amet placeat tenetur ad tempora voluptatum. Facere eaque, quas voluptate accusamus veniam saepe velit iure, adipisci praesentium, fuga quam accusantium?
-        </v-col>
-        <v-col class="pt-16" cols="4">
-            hi hello
-          <template>
-            <!-- <v-card class="mx-auto" style="border-radius: 24px !important; padding: 3px 5px" max-width="300" tile>
-              <v-list style="padding: 0px !important" class="brand">
-                <v-img src="../src/assets/brande.png"></v-img>
-              </v-list>
-              <v-list rounded>
-                <v-list-item-group active-class="active" class="maraginBettwenItems">
-                  <v-list-item>
-                    <router-link to="/home" class="itemd d-flex flex-row" style="text-decoration: none">
-                      <v-list-item-icon>
-                        <v-icon large>mdi-shopping-outline</v-icon>
-                      </v-list-item-icon>
-                      <v-list-item-content>
-                        <v-list-item-title>Products</v-list-item-title>
-                      </v-list-item-content>
-                    </router-link>
-                  </v-list-item>
-                  <v-list-item>
-                    <router-link to="/WishList" class="itemd d-flex flex-row" style="text-decoration: none">
-                      <v-list-item-icon>
-                        <v-icon color="red" large>mdi-cards-heart-outline</v-icon>
-                      </v-list-item-icon>
-                      <v-list-item-content>
-                        <v-list-item-title>WishList</v-list-item-title>
-                      </v-list-item-content>
-                    </router-link>
-                  </v-list-item>
-                  <v-list-item>
-                    <router-link to="/card" class="itemd d-flex flex-row" style="text-decoration: none">
-                      <v-list-item-icon>
-                        <v-icon large>mdi-cart-outline</v-icon>
-                      </v-list-item-icon>
-                      <v-list-item-content>
-                        <v-list-item-title>Cart</v-list-item-title>
-                      </v-list-item-content>
-                    </router-link>
-                  </v-list-item>
-                  <v-divider></v-divider>
-                  <v-list-item>
-                    <router-link to="/profileSetting" class="itemd d-flex flex-column" style="text-decoration: none">
-                      <v-img  class="img" width="70" height="50" v-on:error="logo.url = './assets/brande.png'" :src="
-                        'http://127.0.0.1:8000/storage/' +
-                        useractive[0].picture[0].filename
-                      " v-if="useractive.length > 0"></v-img>
-                      <v-list-item-icon v-else>
-                        <v-icon color="#8eb3b0" x-large>mdi-account-circle</v-icon>
-                      </v-list-item-icon>
-                      <v-list-item-content>
-                        <v-list-item-title v-if="useractive.length > 0">{{
-                            useractive[0].username
-                        }}</v-list-item-title>
-                      </v-list-item-content>
-                    </router-link>
-                  </v-list-item>
-                </v-list-item-group>
-              </v-list>
-            </v-card> -->
-          </template>
-          <!-- </v-sheet> -->
-        </v-col>
-       
-      </v-row>
-    </v-spacer>
+    <v-spacer class="pt-16 px-3"
+        style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif !important;">
+        <v-row>
+            <v-col cols="12">
+                <template>
+                    <v-card elevation="3" class="mx-auto" style="border-radius: 24px !important; padding: 10px 16px;"
+                        max-width="100%" tile>
+                        <v-toolbar-title style="padding-left: 10px;" class="text-h">Card</v-toolbar-title>
+                        <v-divider />
+                        <v-spacer style="padding: 10px 20px;">
+                            <!-- TODO: Here Put your map for map the all list of panel product in v-row -->
+                            <v-row>
+                                <v-col cols="2">
+                                    <v-img class="img" contain
+                                        style="width: 100%;height: 140px !important;background-color: #f6f6f6; background-size: contain !important;"
+                                        src="../../assets/macbook-air-space-gray-select-201810 1.png"></v-img>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-spacer style="padding-top:10px ;padding-right: 5px;">
+                                        <h3>Apple Watch Series 7</h3>
+                                        <h5>1768 DH <v-divider class="mx-2" vertical></v-divider>
+                                            <span class="teal--text lighten-2">IN STOCK</span>
+                                        </h5>
+                                    </v-spacer>
+                                    <v-spacer class="card-about pt-11" style="display: flex;padding-right: 10px; height: 100%;">
+                                        <button v-on:click.prevent="decrement" class="decrement">
+                                            -
+                                        </button>
+                                        <p class="counter">
+                                            {{ count }}
+                                        </p>
+                                        <button v-on:click.prevent="increment" class="increment">
+                                            +
+                                        </button>
+                                    </v-spacer>
+                                </v-col>
+                                <v-col cols="2">
+                                    <v-spacer class="">
+                                        <div class="text-h5 " style="text-align: end;font-weight: 500;color: #7e7777;">
+                                            1768 DH
+                                        </div>
+                                    </v-spacer>
+                                    <v-spacer  style="display: flex;justify-content: flex-end;padding-right: 10px; height: 100%;">
+                                        <!-- <v-action> -->
+                                            <v-icon style="padding-right: 10px; cursor: pointer;">mdi-trash-can-outline</v-icon>
+                                            <v-icon style=" cursor: pointer;">mdi-cards-heart-outline</v-icon>
+                                        <!-- </v-action> -->
+                                    </v-spacer>
+                                </v-col>
+                                <v-divider class="mx-10 " ></v-divider>
+                            </v-row>
+                            
+                        </v-spacer>
+                    </v-card>
+                </template>
+                <!-- </v-sheet> -->
+            </v-col>
 
+        </v-row>
+    </v-spacer>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            dialog: false,
+            count: 1,
+            selection: 2,
+            rating: 3.5,
+            emptyIcon: "mdi-star-outline",
+            fullIcon: "mdi-star",
+            halfIcon: "mdi-star-half",
+            produit: [],
+            idproduit: null,
+        };
+    },
+    methods: {
 
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            if (this.count > 1) {
+                this.count--;
+            }
+        },
+    },
 }
 </script>
+
+
+<style>
+.card-about {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+}
+
+.decrement {
+    width: 24px;
+    height: 24px;
+    border: 2px solid #828ef9;
+    color: #828ef9;
+}
+
+.counter {
+    width: 30px;
+    height: 25px;
+    left: 93px;
+    top: 0px;
+    text-align: center;
+    background: #ffffff;
+    /* border: 2px solid #d4d4d4; */
+    border-radius: 5px;
+    font-family: 'Bevan';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 20px;
+}
+
+.increment {
+    width: 24px;
+    height: 24px;
+    border: 2px solid #828ef9;
+    color: #828ef9;
+}
+</style>
