bca7e10a79cb58c2d8e7aefd5ed302c440c6ac23
MouadAmzil <mouadmouad2026@gmail.com>
2022-09-01 00:43:49 +0100

    Event incremment and decrement

--- src/components/panel/PanelView.vue
+++ src/components/panel/PanelView.vue
@@ -1,76 +1,40 @@
 <template>
-  <v-spacer
-    class="pt-16 px-3"
-    style="
+  <v-spacer class="pt-16 px-3" style="
       font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
         Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
         sans-serif !important;
-    "
-  >
+    ">
     <v-row>
       <v-col cols="8">
         <template>
-          <v-card
-            elevation="3"
-            class="mx-auto"
-            style="border-radius: 24px !important; padding: 10px 16px"
-            max-width="100%"
-            tile
-          >
-            <v-toolbar-title style="padding-left: 10px" class="text-h"
-              >Card</v-toolbar-title
-            >
+          <v-card elevation="3" class="mx-auto" style="border-radius: 24px !important; padding: 10px 16px"
+            max-width="100%" tile>
+            <v-toolbar-title style="padding-left: 10px" class="text-h">Card</v-toolbar-title>
             <v-divider />
-            <v-spacer
-              style="padding: 10px 20px; overflow-y: scroll; height: 73vh"
-            >
+            <v-spacer style="padding: 10px 20px; overflow-y: scroll; height: 73vh">
               <!-- TODO: Here Put your map for map the all list of panel product in v-row -->
-              <div
-                style="padding-left: 180px; padding-top: 100px"
-                v-if="paniers.panier.produits.length == 0"
-              >
-                <lottie-player
-                  src="https://assets9.lottiefiles.com/packages/lf20_3VDN1k.json"
-                  background="transparent"
-                  speed="2"
-                  style="width: 300px; height: 300px"
-                  loop
-                  autoplay
-                ></lottie-player>
+              <div style="padding-left: 180px; padding-top: 100px" v-if="paniers.panier.produits.length == 0">
+                <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_3VDN1k.json" background="transparent"
+                  speed="2" style="width: 300px; height: 300px" loop autoplay></lottie-player>
               </div>
               <div v-else>
-                <v-row
-                  v-for="product in paniers.panier.produits"
-                  :key="product.id"
-                >
+                <v-row v-for="product in paniers.panier.produits" :key="product.id">
                   <v-col cols="3">
-                    <v-img
-                      v-if="product.src==''"
-                      class="img"
-                      contain
-                      style="
+                    <v-img v-if="product.src == ''" class="img" contain style="
                         max-width: 100%;
                         max-height: 200px !important;
                         background-color: #f6f6f6;
                         background-size: contain !important;
-                      "
-                      :src="
+                      " :src="
                         'http://127.0.0.1:8000/storage/' +
                         product.pictures[0].filename
-                      "
-                    ></v-img>
-                    <v-img
-                    v-else
-                      class="img"
-                      contain
-                      style="
+                      "></v-img>
+                    <v-img v-else class="img" contain style="
                         max-width: 100%;
                         max-height: 240px !important;
                         background-color: #f6f6f6;
                         background-size: contain !important;
-                      "
-                      :src="product.src"
-                    ></v-img>
+                      " :src="product.src"></v-img>
                   </v-col>
                   <v-col cols="6">
                     <v-spacer style="padding-top: 10px; padding-right: 5px">
@@ -81,47 +45,38 @@
                         <span class="teal--text lighten-2">IN STOCK</span>
                       </h5>
                     </v-spacer>
-                    <v-spacer
-                      class="card-about pt-11"
-                      style="display: flex; padding-right: 10px; height: 100%"
-                    >
-                      <button v-on:click.prevent="decrement" class="decrement">
+                    <v-spacer class="card-about pt-11" style="display: flex; padding-right: 10px; height: 100%">
+                      <button v-on:click.prevent="decrement(product.id, product.pivot.Qte)" class="decrement">
                         -
                       </button>
                       <p class="counter">
-                        {{ count }}
+                        {{ product.pivot.Qte  }}
                       </p>
-                      <button v-on:click.prevent="increment" class="increment">
+                      <button v-on:click.prevent="increment(product.id)" class="increment">
                         +
                       </button>
                     </v-spacer>
                   </v-col>
                   <v-col cols="3">
                     <v-spacer class="">
-                      <div
-                        class="text-h5"
-                        style="
+                      <div class="text-h5" style="
                           text-align: end;
                           font-weight: 500;
                           color: #7e7777;
-                        "
-                      >
+                        ">
                         {{ product.prix }}DH
                       </div>
                     </v-spacer>
-                    <v-spacer
-                      style="
+                    <v-spacer style="
                         display: flex;
                         justify-content: flex-end;
                         padding-right: 10px;
                         height: 100%;
-                      "
-                    >
+                      ">
                       <!-- <v-action> -->
-                      <v-icon style="padding-right: 10px; cursor: pointer"
-                        >mdi-trash-can-outline
+                      <v-icon style="padding-right: 10px; cursor: pointer">mdi-trash-can-outline
                       </v-icon>
-                      
+
                       <!-- </v-action> -->
                     </v-spacer>
                   </v-col>
@@ -135,20 +90,12 @@
       </v-col>
       <v-col cols="4">
         <template>
-          <v-card
-            elevation="3"
-            class="mx-auto"
-            style="border-radius: 24px !important; padding: 10px 16px"
-            max-width="100%"
-            tile
-          >
-            <v-toolbar-title style="padding-left: 10px" class="text-h4"
-              >Total
+          <v-card elevation="3" class="mx-auto" style="border-radius: 24px !important; padding: 10px 16px"
+            max-width="100%" tile>
+            <v-toolbar-title style="padding-left: 10px" class="text-h4">Total
             </v-toolbar-title>
             <v-divider />
-            <v-spacer
-              style="padding: 10px 20px; overflow-y: scroll; height: 43vh"
-            >
+            <v-spacer style="padding: 10px 20px; overflow-y: scroll; height: 43vh">
               <!-- TODO: Here Put your map for map the all list of panel product in v-row -->
               <v-row class="pa-2 pt-10">
                 <v-col cols="12" class="d-flex flex-row">
@@ -168,10 +115,7 @@
                   <v-spacer></v-spacer>
                   <h4 class="orange--text">{{ shipping + total }}DH</h4>
                 </v-col>
-                <v-col
-                  cols="12"
-                  class="d-flex flex-row justify-center orange--text"
-                >
+                <v-col cols="12" class="d-flex flex-row justify-center orange--text">
                   <v-btn color="green" class="ma-2 white--text">
                     Place order
                     <v-icon right color="#fff"> mdi-cart-arrow-down </v-icon>
@@ -209,9 +153,9 @@ export default {
   }),
   mounted: function () {
     // this.updatelikescount(this.post_id);
-    
+
     //this.initialize();
-   // this.CalcuTotal();
+    // this.CalcuTotal();
   },
   computed: {
     ...mapGetters(["getPaniers", "getUsers", "getUserActive"]),
@@ -228,28 +172,41 @@ export default {
       this.getProduitsPanierByUserAction(this.iduserActive).then(() => {
         this.paniers = this.getPaniers;
         this.paniers.panier.produits.map((res) => {
-              this.total = res.prix + this.total;
-            });
+          this.total = res.prix + this.total;
+        });
       });
     },
-    increment() {
-      this.count++;
+    increment(id) {
+      this.paniers.panier.produits.map((res) => {
+        // debugger;
+        if (res.id == id) {
+          res.pivot.Qte = res.pivot.Qte + 1;
+        }
+      })
+
     },
-    decrement() {
-      if (this.count > 1) {
-        this.count--;
+    decrement(id, QTE) {
+      if (QTE > 1) {
+        this.paniers.panier.produits.map((res) => {
+          if (res.id == id) {
+            res.pivot.Qte = res.pivot.Qte - 1;
+          }
+        })
       }
+      // if (this.count > 1) {
+      //   this.count--;
+      // }
     },
     CalcuTotal() {
-        if (this.paniers.panier.produits.length > 0) {
-            
-            this.paniers.panier.produits.map((res) => {
-              //debugger;
-              this.total = res.prix + this.total;
-            });
-        }else{
-            this.total = 0;
-        }
+      if (this.paniers.panier.produits.length > 0) {
+
+        this.paniers.panier.produits.map((res) => {
+          //debugger;
+          this.total = res.prix + this.total;
+        });
+      } else {
+        this.total = 0;
+      }
     },
   },
 };
